de9b6791eec7fdfa31339e43e68e5e0276ae82ab
Pedro Rocha <pedr0_r@hotmail.com>
2016-08-15 12:13:51 -0300

    gitignore

--- histograma.py
+++
@@ -1,8 +0,0 @@
-his, bins = np.histogram(a, bins = np,arange(256), density=false)
-
-hcc = np.cumsum(his).astype(float)
-hcc = hcc / hcc.max() * 255
-hcc = np-uintu(hcc)
-
-b = np.zeros(a.shape, dtype=np.uint8)
-b = hcc[a]
